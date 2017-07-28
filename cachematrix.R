b723360ef4ac509d91b0e9441eab2ff11614f4c3
bpc5229 <30535071+bpc5229@users.noreply.github.com>
2017-07-28 15:33:44 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+### This function, makeCacheMatrix, creates a special matrix which is a list containing
+### a function to set the value of the matrix, get the value of the matrix,
+### set the value of the inverse, and get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+        x <<- y
+        m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inverse) m <<- inverse 
+      getinverse <- function() m
+      list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+### This function, cacheSolve, returns the inverse of the matrix. If the matrix
+### has already been calculated, then it does not calculate it, but displays the
+### inverse from the cache to save computation resources
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+          message("getting cached data")
+        return(m)
+      }
+      data <- x$get()
+      m <- solve(data)
+      x$setinverse(m)
+      m
 }
+
+### Testing on sample data using the code below shows that the functions
+### work as expected and the cacheSolve does not duplicate the calculation
+### but instead retrieves from the cache if the matrix inverse was already calculated
+
+x = rbind(c(5,10),c(-.5,6))
+x
+matrix = makeCacheMatrix(x)
+matrix$get()
+cacheSolve(matrix)
+cacheSolve(matrix)
